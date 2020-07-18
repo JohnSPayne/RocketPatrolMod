a8c4f0b847a88b4e74c5fa0f7ca0911edeefa6af
JohnSPayne <63931546+JohnSPayne@users.noreply.github.com>
2020-07-17 19:09:21 -0700

    Try Go

---
+++ src/main.js
@@ -0,0 +1,19 @@
+//Student Name: John Payne
+"use strict"
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+}
+
+let game = new Phaser.Game(config);
+
+// reserve some keyboard bindings
+let keyF, keyLEFT, keyRIGHT;
+
+// define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000
+}
